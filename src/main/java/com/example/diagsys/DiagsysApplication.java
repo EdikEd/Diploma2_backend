caf34f98a2d10dacece4f84bc58915f6407f9150
EdikEd <edik.ergart2017@gmail.com>
2023-01-28 00:09:10 +0100

    initial backend spring boot app

---
+++ src/main/java/com/example/diagsys/DiagsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.diagsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagsysApplication.class, args);
+    }
+
+}
